1bdc66dd90191a70599385684d934ff9f376dd4e
cappeLindo <vitorcappe@gmail.com>
2025-05-14 14:23:37 -0400

    criando loops que leem minha msg por byte

---
+++ variaveis/string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() { 
+	s := "Olá mundo!"
+
+	for _, v := range s { 
+		fmt.Printf("%v - %T - %#U - %#x \n", v, v, v, v)
+	}
+	fmt.Println("")
+	
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%v - %T - %#U - %#x \n", s[i], s[i], s[i], s[i])
+	}
+}
