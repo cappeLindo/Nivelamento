d573ff1918058c5d6c67b225385ddf5964a334e3
cappeLindo <vitorcappe@gmail.com>
2025-05-15 18:48:54 -0400

    add bits

---
+++ variaveis/bits.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const (
+	_ = iota 
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+	tb = 1 << (iota * 10)
+)
+
+func main() { 
+
+	fmt.Println("Binário\t\t\t\t\t\t\tDecimal")
+	fmt.Printf("Bytes: %b\t\t\t\t\tDecimal: %d\n", kb, kb)
+	fmt.Printf("Bytes: %b\t\t\t\tDecimal: %d\n", mb, mb)
+	fmt.Printf("Bytes: %b\t\t\tDecimal: %d\n", gb, gb)
+	fmt.Printf("Bytes: %b\tDecimal: %d\n", tb, tb)
+	
+}
