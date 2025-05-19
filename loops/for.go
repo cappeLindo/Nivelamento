47441db828556c16ad9ecdd61ed2244f50003f01
cappeLindo <vitorcappe@gmail.com>
2025-05-19 17:55:43 -0400

    new tasks

--- loops/for.go
+++ loops/for.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
-func main() { 
+func main() {
 
-	for x := 1; x <= 10; x++ { 
+	/*for x := 1; x <= 10; x++ {
 		fmt.Println(x)
+	}*/
+
+	i := 1
+
+	for {
+		if i <= 10 {
+			fmt.Println("i é menor que dez", i)
+			i++
+		} else {
+			fmt.Println("i é maior que dez!")
+			break
+		}
 	}
+	fmt.Println("O loop foi brekado!")
+	
 
 }
